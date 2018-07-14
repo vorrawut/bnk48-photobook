e428f89080c5fdcf486e0c19a157b0eda1c3c670
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-14 14:55:22 +0700

    add routeLink to rount by id in app.module and bnk-girl-component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 
+// Component
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { BnkGirlListComponent } from './component/bnk-girl-list/bnk-girl-list.component';
 import { BnkGirlComponent } from './component/bnk-girl/bnk-girl.component';
+import { InstagramComponent } from './component/instagram/instagram.component';
 
+// Route
 const routes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'instagram/:instagramId', component: InstagramComponent }
 ];
 
 @NgModule({
@@ -19,7 +23,8 @@ const routes: Route[] = [
     AppComponent,
     DashboardComponent,
     BnkGirlListComponent,
-    BnkGirlComponent
+    BnkGirlComponent,
+    InstagramComponent
   ],
   imports: [
     BrowserModule,
