888f8250f5a2df06b78707cf651817accba2aa80
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-14 14:43:45 +0700

    add routingModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { BnkGirlListComponent } from './component/bnk-girl-list/bnk-girl-list.component';
 import { BnkGirlComponent } from './component/bnk-girl/bnk-girl.component';
+
+const routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { BnkGirlComponent } from './component/bnk-girl/bnk-girl.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
