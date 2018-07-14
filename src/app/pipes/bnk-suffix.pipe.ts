8341b7634121a3bbad5af283200999b938eec5fc
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-14 16:48:04 +0700

    add /BNK48 to BNKSuffixPipe

--- src/app/pipes/bnk-suffix.pipe.ts
+++ src/app/pipes/bnk-suffix.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BnkSuffixPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: String, args?: any): any {
+    return value.toUpperCase() + '/BNK48';
   }
 
 }
